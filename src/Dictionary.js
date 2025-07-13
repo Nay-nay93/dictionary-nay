647396de7e78c826979066f3a752fc6fdea82834
Nay-nay93 <greconaomi@gmail.com>
2025-07-13 21:06:23 +0100

    added results.js and meaning.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,19 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState({});
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        console.log(response.data);
+        setResults(response.data);
     }
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
 
         let apiKey = "13c9444ao05d100014e814t7f46bd8da";
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
@@ -28,6 +30,7 @@ export default function Dictionary() {
         <form onSubmit={search}>
             <input type="search" id="dictionary-search" onChange= {handleKeywordChange} />
         </form>
+        <Results results={results} />
     </div>
     );
 }
