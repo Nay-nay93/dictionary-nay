dda5eccede530b83b63445b2ca6f9a3b1c7aac50
Nay-nay93 <greconaomi@gmail.com>
2025-07-13 23:20:36 +0100

    styling fixes

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,16 @@
 import React from "react";
+import "./Meaning.css"
 
 export default function Meaning(props) {
     console.log(props.meaning);
     return (
-      <div className="Meaning card my-3 p-3">
+      <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
         <div>
-            <p>
+            <p >
                 {props.meaning.definition}
                 <br />
-                <em>{props.meaning.example}</em>
+                <em className="example">{props.meaning.example}</em>
             </p>
         </div>
       </div>
