949960dd6a561a2794f544f330d55f57e9cdfb20
Nay-nay93 <greconaomi@gmail.com>
2025-07-14 22:13:10 +0100

    added phonetics and styled

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Meaning from "./Meaning";
-import "./Results.css"
+import "./Results.css";
 
 export default function Results(props) {
-
     if (props.results) {
        return (
-         <div className="Results container text-center my-4">
+         <div className="Results container text-center text-capitalize my-4">
                 <h2>{props.results.word}</h2>
+                <div className="Phonetics">{props.results.phonetic}</div>
                 {props.results.meanings.map(function (meaning, index) {
                    return (
                      <div className="row justify-content-center"  key={index}>
